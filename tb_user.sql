5d6306fb262cbf7e60716b009a75e010e0f950c4
Sama Ahmed Salem <161253203+Sama-Ahmed-Salem@users.noreply.github.com>
2024-12-21 17:23:49 +0200

    Update tb_user.sql

--- tb_user.sql
+++ tb_user.sql
@@ -1,16 +1,16 @@
-
 -- phpMyAdmin SQL Dump
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 25, 2024 at 02:27 PM
+-- Generation Time: Dec 18, 2024 at 03:23 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -28,92 +28,48 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `tb_user` (
-  `name` VARCHAR(25) NOT NULL,
-  `email` VARCHAR(50) NOT NULL,
-  `password` VARCHAR(75) NOT NULL,
-  `role` VARCHAR(10) NOT NULL DEFAULT 'user',
-  `role` VARCHAR(10) NOT NULL DEFAULT 'user',
-  `feedback` TEXT NULL,
-  PRIMARY KEY (`name`)
+  `id` int(11) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(75) NOT NULL,
+  `role` varchar(10) NOT NULL DEFAULT 'user',
+  `feedback` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `tb_user`
 --
 
-INSERT INTO `tb_user` (`name`, `email`, `password`,`role`,  `feedback`) VALUES
-('jana', 'jana1@gmail.com', PASSWORD('jana123'), 'admin',NULL),
-('sama', 'sama1@gmail.com', PASSWORD('sama123'), 'user',NULL),
-('mostafa', 'mostafa1@gmail.com', PASSWORD('mostafa123'), 'user',NULL),
-('habiba', 'habiba1@example.com', PASSWORD('habiba123'), 'user',NULL),
-('shaaban', 'shaaban1@gmail.com', PASSWORD('shaaban123'), 'user',NULL);
-INSERT INTO `tb_user` (`name`, `email`, `password`,`role`, `feedback`) VALUES
-('jana', 'jana1@gmail.com', PASSWORD('jana123'),'admin', NULL),
-('sama', 'sama1@gmail.com', PASSWORD('sama123'),'user', NULL),
-('mostafa', 'mostafa1@gmail.com', PASSWORD('mostafa123'), 'user',NULL),
-('habiba', 'habiba1@example.com', PASSWORD('habiba123'),'user', NULL),
-('shaaban', 'shaaban1@gmail.com', PASSWORD('shaaban123'), 'user',NULL);
+INSERT INTO `tb_user` (`id`, `name`, `email`, `password`, `role`, `feedback`) VALUES
+(1, 'dr.esraa', 'ahla.dr.esraa1@gmail.com', PASSWORD('dresraa123'), 'user', 'Dr Esraa top el top'),
+(2, 'habiba', 'habiba1@example.com', PASSWORD('habiba123'), 'user', NULL),
+(3, 'jana', 'jana1@gmail.com', PASSWORD('jana123'), 'admin', NULL),
+(4, 'mounib', 'mostafa1@gmail.com', PASSWORD('mostafa123'), 'user', NULL),
+(5, 'sama', 'sama1@gmail.com', PASSWORD('sama123'), 'user', 'great'),
+(6, 'shabaan', 'shaaban1@gmail.com', PASSWORD('shaaban123'), 'user', 'wowwwww'),
+(7, 'test', 'test1@gmail.com', PASSWORD('test123'), 'user', NULL);
 
--- --------------------------------------------------------
+--
+-- Indexes for dumped tables
+--
 
+--
+-- Indexes for table `tb_user`
+--
+ALTER TABLE `tb_user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tb_user`
+--
+ALTER TABLE `tb_user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-=======
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Nov 25, 2024 at 02:27 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `reglog`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tb_user`
---
-
-CREATE TABLE `tb_user` (
-  `name` VARCHAR(25) NOT NULL,
-  `email` VARCHAR(50) NOT NULL,
-  `password` VARCHAR(75) NOT NULL,
-  `feedback` TEXT NULL,
-  PRIMARY KEY (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `tb_user`
---
-
-INSERT INTO `tb_user` (`name`, `email`, `password`, `feedback`) VALUES
-('jana', 'jana1@gmail.com', PASSWORD('jana123'), NULL),
-('sama', 'sama1@gmail.com', PASSWORD('sama123'), NULL),
-('mostafa', 'mostafa1@gmail.com', PASSWORD('mostafa123'), NULL),
-('habiba', 'habiba1@example.com', PASSWORD('habiba123'), NULL),
-('shaaban', 'shaaban1@gmail.com', PASSWORD('shaaban123'), NULL);
-
--- --------------------------------------------------------
-
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
